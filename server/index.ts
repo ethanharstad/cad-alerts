e20e134abd13fdbf1bb697cdc07bc542c8661633
Ethan Harstad <ethanharstad@gmail.com>
2025-11-09 22:09:15 -0600

    Try import

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,9 @@
 import { app } from './api';
 import { emailHandler } from './email-handler';
+import AlertWorkflow from './workflow';
 
 export default {
 	fetch: app.fetch,
 	email: emailHandler,
+	workflow: AlertWorkflow,
 }
